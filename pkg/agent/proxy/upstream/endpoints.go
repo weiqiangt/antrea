11aef4630a3241601f1795ec11bf6a63d19079c6
Weiqiang TANG <weiqiangt@vmware.com>
2020-06-18 11:09:07 +0800

    Update pkg/agent/openflow/pipeline.go
    Co-authored-by: Quan Tian <qtian@vmware.com>
    Signed-off-by: Weiqiang TANG <weiqiangt@vmware.com>

--- pkg/agent/proxy/upstream/endpoints.go
+++ pkg/agent/proxy/upstream/endpoints.go
@@ -13,7 +13,30 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+/*
+// Copyright 2020 Antrea Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
+Modifies:
+- Remove imports: "net", "reflect", "strconv", "sync", "time", "k8s.io/api/core/v1",
+  "k8s.io/api/discovery/v1beta1", "k8s.io/apimachinery/pkg/util/sets",
+  "k8s.io/client-go/tools/record", "k8s.io/klog", "k8s.io/utils/net"
+- Remove vars: "supportedEndpointSliceAddressTypes"
+- Remove functions: "newBaseEndpointInfo", "makeEndpointFunc",
+  "NewEndpointChangeTracker", "detectStaleConnections"
+- Remove structs: "EndpointChangeTracker", "EndpointsMap"
+*/
 package upstream
 
 import (
--- pkg/agent/proxy/upstream/types.go
+++ pkg/agent/proxy/upstream/types.go
@@ -13,6 +13,25 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+/*
+// Copyright 2020 Antrea Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+Modifies:
+- Remove interface "Provider"
+- Remove import "k8s.io/kubernetes/pkg/proxy/config"
+*/
 
 package upstream
 
@@ -67,6 +86,7 @@ type ServicePort interface {
 }
 
 // Endpoint in an interface which abstracts information about an endpoint.
+// TODO: Rename functions to be consistent with ServicePort.
 type Endpoint interface {
 	// String returns endpoint string.  An example format can be: `IP:Port`.
 	// We take the returned value as ServiceEndpoint.Endpoint.
